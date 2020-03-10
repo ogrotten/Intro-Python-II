5bc0e8791e7bb5085e2cf19310221343adcfa475
chip <chiposaur@gmail.com>
2020-03-10 18:27:48 -0500

    getting started

--- src/player.py
+++ src/player.py
@@ -1,2 +1,19 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Mob:
+	def __init__(self, kind, location):
+		self.type, self.location, self.inv = kind, location, inv
+
+	def kill():
+		return (f"{type} is dead.")
+
+class Player(Mob):
+	def __init__(self, name, inv):
+		super().__init__(kind, location)
+		self.name, self.inv = name, inv
+	def move(dir):
+		# get direction
+		# move only if that direction is available.
+	self.name = "The Player"
+	self.kind = "player"
